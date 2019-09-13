e23bb2748b16b4f561ad9b0207905162838f2046
Maksim Shabanski <shabanskij.maksim@gmail.com>
2019-09-14 00:07:44 +0300

    feat: add handler for buttons of slider

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -9,6 +9,35 @@ const ANIMATION_DURATION = 500;
 class Slider extends Component {
   state = {
     activePage: 1,
+    isAnimated: false,
+  }
+
+  handleBtnClick = (e) => {
+    const { btn } = e.target.dataset;
+    let { activePage, isAnimated } = this.state;
+
+    if (isAnimated) {
+      return null;
+    }
+  
+    if (btn === 'next') {
+      activePage += 1;
+    } else {
+      activePage -= 1;
+    }
+    
+    this.setState({
+      activePage,
+      isAnimated: true,
+    });
+
+    this.turnOffAnimation();
+  }
+
+  turnOffAnimation() {
+    setTimeout(() => {
+      this.setState({ isAnimated: false });
+    }, ANIMATION_DURATION);
   }
 
   render() {
@@ -26,6 +55,8 @@ class Slider extends Component {
       transition: `transform ${ANIMATION_DURATION}ms ease-in-out 0s`,
       transform: `translate3d(-${scrollPos}px, 0px, 0px)`,
     };
+    const disabledPrevBtn = activePage === 1 ? true : false;
+    const disabledNextBtn = videosData.length < TOTAL_CARDS_ON_SLIDES ? true : false;
 
     return (
       <div className="slider">
@@ -35,9 +66,23 @@ class Slider extends Component {
           ))}
         </div>
         <div className="slider__controls">
-          <button className="slider__btn" disabled>Prev</button>
+          <button
+            data-btn="prev"
+            className="slider__btn"
+            disabled={disabledPrevBtn}
+            onClick={this.handleBtnClick}
+          >
+            Prev
+          </button>
           <span className="slider__current-page">{activePage}</span>
-          <button className="slider__btn" disabled>Next</button>
+          <button
+            data-btn="next"
+            className="slider__btn"
+            disabled={disabledNextBtn}
+            onClick={this.handleBtnClick}
+          >
+            Next
+          </button>
         </div>
       </div>
     );
