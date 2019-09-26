57dae28bd50704b6d016189ba3984f2bc3109084
Maksim Shabanski <shabanskij.maksim@gmail.com>
2019-09-27 00:36:00 +0300

    Updates

---
+++ precache-manifest.a36efc4a388a291f27ba6c21bb9bb850.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c6e01bf1c961dff7d61b32d84102fed",
+    "url": "/youtube-client/index.html"
+  },
+  {
+    "revision": "8f4f8e3ffb5c2efa899f",
+    "url": "/youtube-client/static/css/main.1d07291f.chunk.css"
+  },
+  {
+    "revision": "e9434674972ad73901c8",
+    "url": "/youtube-client/static/js/2.ca490870.chunk.js"
+  },
+  {
+    "revision": "8f4f8e3ffb5c2efa899f",
+    "url": "/youtube-client/static/js/main.f8e9ab29.chunk.js"
+  },
+  {
+    "revision": "bbc59d053f53516036d8",
+    "url": "/youtube-client/static/js/runtime~main.77238eec.js"
+  }
+]);
