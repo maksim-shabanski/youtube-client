17d1341cd8114437c9bb2a8689fd329b71f5a9c8
Maksim Shabanski <shabanskij.maksim@gmail.com>
2019-10-05 21:28:50 +0300

    feat: add propTypes and some improve for Slider component

--- src/components/Slider/index.js
+++ src/components/Slider/index.js
@@ -1,8 +1,38 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Spinner from 'components/Spinner';
 import Card from 'components/Card';
 import { ANIMATION_DURATION, CARD_WIDTH } from 'utilities/constants';
 import './slider.scss';
+
+const propTypes = {
+  videosData: PropTypes.arrayOf(PropTypes.object).isRequired,
+  selectedSlide: PropTypes.number,
+  totalSlides: PropTypes.number,
+  totalCardsOnSlide: PropTypes.number,
+  isExistMoreSlides: PropTypes.bool,
+  onClick: PropTypes.func,
+  onMouseDown: PropTypes.func,
+  onMouseMove: PropTypes.func,
+  onMouseUp: PropTypes.func,
+  onTouchStart: PropTypes.func,
+  onTouchMove: PropTypes.func,
+  onTouchEnd: PropTypes.func,
+};
+
+const defaultProps = {
+  selectedSlide: 1,
+  totalSlides: 1,
+  totalCardsOnSlide: 1,
+  isExistMoreSlides: true,
+  onClick: () => {},
+  onMouseDown: () => {},
+  onMouseMove: () => {},
+  onMouseUp: () => {},
+  onTouchStart: () => {},
+  onTouchMove: () => {},
+  onTouchEnd: () => {},
+};
 
 const Slider = ({
   videosData,
@@ -26,8 +56,8 @@ const Slider = ({
     transition: `transform ${ANIMATION_DURATION}ms ease-in-out 0s`,
     transform: `translate3d(-${scrollPos}px, 0px, 0px)`,
   };
-  const isDisabledPrevBtn = selectedSlide === 1 ? true : false;
-  const isDisabledNextBtn = selectedSlide === totalSlides ? true : false;
+  const isDisabledPrevBtn = selectedSlide === 1;
+  const isDisabledNextBtn = selectedSlide === totalSlides;
 
   let nextBtnCaption = 'Next';
   if (isExistMoreSlides && selectedSlide === totalSlides ) {
@@ -37,7 +67,8 @@ const Slider = ({
   return (
     <div className="slider">
       <div 
-        className="slider__track" style={sliderTrackStyles}
+        className="slider__track"
+        style={sliderTrackStyles}
         onMouseDown={onMouseDown}
         onMouseMove={onMouseMove}
         onMouseUp={onMouseUp}
@@ -71,5 +102,8 @@ const Slider = ({
     </div>
   );
 }
+
+Slider.propTypes = propTypes;
+Slider.defaultProps = defaultProps;
 
 export default Slider;
